c7111ac585765eea946bb03cf3b325c75d7e5977
sfq-1 <sfq1@cdc.gov>
2025-09-05 12:00:37 -0400

    added in get_credible_interval function

--- R/medians.R
+++ R/medians.R
@@ -10,7 +10,7 @@ get_medians <- function(output) {
 #' Calculate Relative Precisions
 #' @param output Array of output to generate relative precisions for.
 #' @param medians Array of medians generated from output
-#' @param perc_ci Number from 0 to 1. Generates relative precisions for a \code{perc_ci * 100}\% credible interval.
+#' @param perc_ci Number from 0 to 1. Determines width of credible interval.
 #'
 #' @export
 get_relative_precision <- function(output, medians, perc_ci = 0.95) {
@@ -19,4 +19,15 @@ get_relative_precision <- function(output, medians, perc_ci = 0.95) {
   hi    <- apply(output, 1:ndims, stats::quantile, 1 - alpha)
   lo    <- apply(output, 1:ndims, stats::quantile, alpha)
   medians / (hi - lo)
+}
+
+#' Calculate Credible Intervals
+#' @param output Array of output to generate relative precisions for.
+#' @param perc_ci Number from 0 to 1. Determines width of credible interval.
+#'
+#' @export
+get_credible_interval <- function(output, perc_ci = 0.95) {
+  ndims <- length(dim(output)) - 1
+  alpha <- (1 - perc_ci) / 2
+  apply(output, 1:ndims, stats::quantile, c(alpha, 1 - alpha))
 }
