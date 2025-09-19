57d6fc2f40e6b9348a299f21e2f14a7182ea78af
sfq-1 <sfq1@cdc.gov>
2025-09-19 15:50:35 -0400

    updated documentation

--- R/logit_expit.R
+++ R/logit_expit.R
@@ -1,6 +1,7 @@
 #' Logit transformation
 #'
 #' @param x the value to be logit-transformed
+#' @noRd
 logit <- function(x) {
   log(x / (1 - x))
 }
@@ -8,6 +9,7 @@ logit <- function(x) {
 #' Expit transformation
 #'
 #' @param x the value to be expit-transformed
+#' @noRd
 expit <- function(x) {
   1 / (1 + exp(-x))
 }
