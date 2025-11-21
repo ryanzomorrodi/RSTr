7e90cc30f5f6dc9d238b1372e8fe46bf56b6772e
sfq-1 <sfq1@cdc.gov>
2025-11-21 11:03:10 -0500

    changed names of adjacency and initialization vignettes

--- R/check_spatial_data.R
+++ R/check_spatial_data.R
@@ -9,7 +9,7 @@ check_spatial_data <- function(adjacency) {
   # Regions have no neighbors
   if (any(sapply(adjacency, length) == 0)) {
     errct <- errct + 1
-    errtxt <- paste(errct, ": Some regions have no neighbors. Ensure all regions have at least 1 neighbor. Check vignette('RSTr-adj') for more information")
+    errtxt <- paste(errct, ": Some regions have no neighbors. Ensure all regions have at least 1 neighbor. Check vignette('RSTr-adjacency') for more information")
     errout <- c(errout, errtxt)
   }
   if (errct) {
